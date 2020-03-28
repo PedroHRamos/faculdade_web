830d030b05a9165e384b26a84742846972b75de0
Luan Barbosa <luan.barbosa.lara@gmail.com>
2020-03-28 16:39:25 -0300

    Aluno pipe e component

---
+++ src/app/pipes/aluno.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { AlunoPipe } from './aluno.pipe';
+
+describe('AlunoPipe', () => {
+  it('create an instance', () => {
+    const pipe = new AlunoPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
