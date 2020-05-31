6bf100a63ea91e71948fcdc135b2fdbf39fbfe55
Luan Barbosa <luan.barbosa.lara@gmail.com>
2020-05-31 19:43:43 -0300

    busca viva home implementada

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,10 @@
 import { InstituicaoService } from './../../services/instituicao.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { UsuarioService } from 'src/app/services/usuario.service';
 import { FormGroup } from '@angular/forms';
 import { InstituicaoDTO } from 'src/app/dto/instituicaoDTO';
+import { Router } from '@angular/router';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-home',
@@ -13,10 +15,12 @@ export class HomeComponent implements OnInit {
 
   constructor(
     private usuarioService: UsuarioService,
-    private instituicaoService: InstituicaoService) {}
+    private instituicaoService: InstituicaoService,
+    private route: Router) {}
 
   instituicoes: Array<InstituicaoDTO>;
-  strInstituicao: string;
+  instituicoesFiltradas: Array<InstituicaoDTO>;
+  strInstituicao = '';
 
   ngOnInit(): void {
     this.obterInstituicoes();
@@ -29,7 +33,15 @@ export class HomeComponent implements OnInit {
   }
 
   pesquisaInstituicao() {
-    
+    if (this.strInstituicao.length > 2) {
+      this.instituicoesFiltradas = this.instituicoes.filter(x => this.strInstituicao.toLowerCase() === x.nome.toLowerCase());
+    }
+  }
+
+  detalheInstituicao(id: number) {
+    this.instituicaoService.obterInstituicao(id).subscribe(res => {
+      this.route.navigate(['/detalhe-faculdade'], {state: {instituicao: res}});
+    });
   }
 
 }
