e23c98addcff19d7c48031e2c6dc20d3f78e1112
Tiago Dantas <tiagodantas42@gmail.com>
2020-06-05 17:07:08 -0300

    Rank "ordenado"

--- src/app/pages/rank/rank.component.ts
+++ src/app/pages/rank/rank.component.ts
@@ -28,8 +28,25 @@ export class RankComponent implements OnInit {
 
   detalheInstituicao(id: number) {
     this.instituicaoService.obterInstituicao(id).subscribe(res => {
-      this.route.navigate(['/detalhe-faculdade'], {state: {instituicao: res}});
+      this.route.navigate(['/detalhe-faculdade'], { state: { instituicao: res } });
     });
+  }
+
+  // Inicio Ordenação Nome
+  ordernarNomeStart() {
+    var valor = document.getElementById('nome').innerHTML;
+    if (valor === 'Nome') {
+      document.getElementById('nome').innerHTML = 'Nome ▲';
+      this.ordernarNome();
+    } else {
+      if (valor === 'Nome ▲') {
+        document.getElementById('nome').innerHTML = 'Nome ▼';
+        this.ordernarNome2();
+      } else {
+        document.getElementById('nome').innerHTML = 'Nome ▲';
+        this.ordernarNome();
+      }
+    }
   }
 
   ordernarNome() {
@@ -48,10 +65,44 @@ export class RankComponent implements OnInit {
     console.log(this.instiuicoes.sort(compare));
   }
 
+  ordernarNome2() {
+    function compare(a, b) {
+      const bandA = a.nome.toUpperCase();
+      const bandB = b.nome.toUpperCase();
+
+      let comparison = 0;
+      if (bandA > bandB) {
+        comparison = -1;
+      } else if (bandA < bandB) {
+        comparison = 1;
+      }
+      return comparison;
+    }
+    console.log(this.instiuicoes.sort(compare));
+  }
+
+  // Fim Ordenação Nome
+  // Inicio Ordenação Professor
+  ordernarProfesssorStart() {
+    var valor = document.getElementById('professores').innerHTML;
+    if (valor === 'Professores') {
+      document.getElementById('professores').innerHTML = 'Professores ▲';
+      this.ordernarProfessor();
+    } else {
+      if (valor === 'Professores ▲') {
+        document.getElementById('professores').innerHTML = 'Professores ▼';
+        this.ordernarProfessor2();
+      } else {
+        document.getElementById('professores').innerHTML = 'Professores ▲';
+        this.ordernarProfessor();
+      }
+    }
+  }
+
   ordernarProfessor() {
     function compare(a, b) {
-      const bandA = a.professor.toUpperCase();
-      const bandB = b.professor.toUpperCase();
+      const bandA = a.professor;
+      const bandB = b.professor;
 
       let comparison = 0;
       if (bandA > bandB) {
@@ -63,4 +114,227 @@ export class RankComponent implements OnInit {
     }
     console.log(this.instiuicoes.sort(compare));
   }
+
+  ordernarProfessor2() {
+    function compare(a, b) {
+      const bandA = a.professor;
+      const bandB = b.professor;
+
+      let comparison = 0;
+      if (bandA > bandB) {
+        comparison = -1;
+      } else if (bandA < bandB) {
+        comparison = 1;
+      }
+      return comparison;
+    }
+    console.log(this.instiuicoes.sort(compare));
+  }
+
+  // Fim Ordenação Professor
+  // Inicio Ordenação Rank
+
+  ordernarRankStart() {
+    var valor = document.getElementById('rank').innerHTML;
+    if (valor === 'Rank') {
+      document.getElementById('rank').innerHTML = 'Rank ▲';
+      this.ordernarRank();
+    } else {
+      if (valor === 'Rank ▲') {
+        document.getElementById('rank').innerHTML = 'Rank ▼';
+        this.ordernarRank2();
+      } else {
+        document.getElementById('rank').innerHTML = 'Rank ▲';
+        this.ordernarRank();
+      }
+    }
+  }
+
+  ordernarRank() {
+    function compare(a, b) {
+      const bandA = a.rank;
+      const bandB = b.rank;
+
+      let comparison = 0;
+      if (bandA > bandB) {
+        comparison = 1;
+      } else if (bandA < bandB) {
+        comparison = -1;
+      }
+      return comparison;
+    }
+    console.log(this.instiuicoes.sort(compare));
+  }
+
+  ordernarRank2() {
+    function compare(a, b) {
+      const bandA = a.rank;
+      const bandB = b.rank;
+
+      let comparison = 0;
+      if (bandA > bandB) {
+        comparison = -1;
+      } else if (bandA < bandB) {
+        comparison = 1;
+      }
+      return comparison;
+    }
+    console.log(this.instiuicoes.sort(compare));
+  }
+
+  // Fim Ordenação Rank
+  // Inicio Ordenação Media
+
+  ordernarMediaStart() {
+    var valor = document.getElementById('media').innerHTML;
+    if (valor === 'Avaliação geral média') {
+      document.getElementById('media').innerHTML = 'Avaliação geral média ▲';
+      this.ordernarMedia();
+    } else {
+      if (valor === 'Avaliação geral média ▲') {
+        document.getElementById('media').innerHTML = 'Avaliação geral média ▼';
+        this.ordernarMedia2();
+      } else {
+        document.getElementById('media').innerHTML = 'Avaliação geral média ▲';
+        this.ordernarMedia();
+      }
+    }
+  }
+
+  ordernarMedia() {
+    function compare(a, b) {
+      const bandA = a.media;
+      const bandB = b.media;
+
+      let comparison = 0;
+      if (bandA > bandB) {
+        comparison = 1;
+      } else if (bandA < bandB) {
+        comparison = -1;
+      }
+      return comparison;
+    }
+    console.log(this.instiuicoes.sort(compare));
+  }
+
+  ordernarMedia2() {
+    function compare(a, b) {
+      const bandA = a.media;
+      const bandB = b.media;
+
+      let comparison = 0;
+      if (bandA > bandB) {
+        comparison = -1;
+      } else if (bandA < bandB) {
+        comparison = 1;
+      }
+      return comparison;
+    }
+    console.log(this.instiuicoes.sort(compare));
+  }
+
+  // Fim Ordenação Media
+  // Inicio Ordenação Infraestrutura
+
+  ordernarInfraestruturaStart() {
+    var valor = document.getElementById('infraestrutura').innerHTML;
+    if (valor === 'Infraestrutura') {
+      document.getElementById('infraestrutura').innerHTML = 'Infraestrutura ▲';
+      this.ordernarInfraestrutura();
+    } else {
+      if (valor === 'Infraestrutura ▲') {
+        document.getElementById('infraestrutura').innerHTML = 'Infraestrutura ▼';
+        this.ordernarInfraestrutura2();
+      } else {
+        document.getElementById('infraestrutura').innerHTML = 'Infraestrutura ▲';
+        this.ordernarInfraestrutura();
+      }
+    }
+  }
+
+  ordernarInfraestrutura() {
+    function compare(a, b) {
+      const bandA = a.infraestrutura;
+      const bandB = b.infraestrutura;
+
+      let comparison = 0;
+      if (bandA > bandB) {
+        comparison = 1;
+      } else if (bandA < bandB) {
+        comparison = -1;
+      }
+      return comparison;
+    }
+    console.log(this.instiuicoes.sort(compare));
+  }
+
+  ordernarInfraestrutura2() {
+    function compare(a, b) {
+      const bandA = a.infraestrutura;
+      const bandB = b.infraestrutura;
+
+      let comparison = 0;
+      if (bandA > bandB) {
+        comparison = -1;
+      } else if (bandA < bandB) {
+        comparison = 1;
+      }
+      return comparison;
+    }
+    console.log(this.instiuicoes.sort(compare));
+  }
+
+  // Fim Ordenação Infraestrutura
+  // Inicio Ordenação Cordenação
+
+  ordernarCordenacaoStart() {
+    var valor = document.getElementById('cordenacao').innerHTML;
+    if (valor === 'Cordenação') {
+      document.getElementById('cordenacao').innerHTML = 'Cordenação ▲';
+      this.ordernarCordenacao();
+    } else {
+      if (valor === 'Cordenação ▲') {
+        document.getElementById('cordenacao').innerHTML = 'Cordenação ▼';
+        this.ordernarCordenacao2();
+      } else {
+        document.getElementById('cordenacao').innerHTML = 'Cordenação ▲';
+        this.ordernarCordenacao();
+      }
+    }
+  }
+
+  ordernarCordenacao() {
+    function compare(a, b) {
+      const bandA = a.cordenacao;
+      const bandB = b.cordenacao;
+
+      let comparison = 0;
+      if (bandA > bandB) {
+        comparison = 1;
+      } else if (bandA < bandB) {
+        comparison = -1;
+      }
+      return comparison;
+    }
+    console.log(this.instiuicoes.sort(compare));
+  }
+
+  ordernarCordenacao2() {
+    function compare(a, b) {
+      const bandA = a.cordenacao;
+      const bandB = b.cordenacao;
+
+      let comparison = 0;
+      if (bandA > bandB) {
+        comparison = -1;
+      } else if (bandA < bandB) {
+        comparison = 1;
+      }
+      return comparison;
+    }
+    console.log(this.instiuicoes.sort(compare));
+  }
+
+  // Fim Ordenação Cordenação
+
 }
